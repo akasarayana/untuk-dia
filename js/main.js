012cb25cec1d8a8da051ade577698f6cb50852df
akasarayana <ilyaspashamirdiansyah@gmail.com>
2025-08-30 12:47:05 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = (' IM SORRY ').split('')
+    const titles = (' MAAFIN GW YAA ').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
